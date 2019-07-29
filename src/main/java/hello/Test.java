7faacd470a4f33caf5d46f0146ac4f5bb058f322
hi2sanjay <hi2sanjay@gmail.com>
2019-07-29 23:46:05 +0100

    again test.java

--- src/main/java/hello/Test.java
+++ src/main/java/hello/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
     public  void test()
     {
+        System.out.println("Test one");
         System.out.println("Test two");
+        System.out.println("Test three");
     }
 }
